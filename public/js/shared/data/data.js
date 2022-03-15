f5a508f9a6fed65b5b7e0efd7d9aa3ad33ec7419
mariusz5581 <mariusz5581@gmail.com>
2022-03-15 21:18:50 +0200

    update of glc.png

--- public/js/shared/data/data.js
+++ public/js/shared/data/data.js
@@ -24,7 +24,7 @@ function getRandomInt(min, max) {
     }
     return result;
 }
-var partIndex = 100000;
+/*var partIndex = 100000;
 var parts = [];
 for (var i = 0; i < 100000; i++) {
     parts[i] = new Parts();
@@ -39,24 +39,18 @@ for (var i = 0; i < 100000; i++) {
     parts[i].y = y;
 
 }
-console.log(parts);
-/*
-parts[0].data = 'nr:a1776376500|x:615|y:375';
-parts[1].data = 'nr:a1776201100|x:437|y:359';
-parts[2].data = 'nr:a1778810500|x:576|y:400';
-parts[3].data = 'nr:a1776264400|x:438|y:381';
-parts[4].data = 'nr:a1776104500|x:492|y:360';
-parts[5].data = 'nr:a1776104600|x:492|y:360';
-parts[6].data = 'nr:a1776110200|x:551|y:357';
-parts[7].data = 'nr:a1776103900|x:551|y:357';
-parts[8].data = 'nr:a1776207500|x:467|y:372';
-parts[9].data = 'nr:a1776101802|x:526|y:366';
-parts[10].data = 'nr:a1776370200|x:582|y:357';
-parts[11].data = 'nr:a1776303600|x:568|y:367';
-parts[12].data = 'nr:a1778810600|x:576|y:408';
-parts[13].data = 'nr:a1776375900|x:560|y:364';
-parts[14].data = 'nr:a1776103200|x:504|y:404';
-parts[15].data = 'nr:a1776370100|x:608|y:356';
-parts[16].data = 'nr:a1776307700|x:560|y:364';
-parts[17].data = 'nr:a1776206201|x:437|y:370';
-*/
+console.log(parts);*/
+
+//ALUE 81
+//Korilinja
+parts[0].data = 'nr:a0009874744|x:384|y:336';
+parts[0].data = 'nr:a0009875144|x:384|y:336';
+parts[0].data = 'nr:a0009908411|x:384|y:336';
+parts[0].data = 'nr:a0009912903|x:384|y:336';
+parts[0].data = 'nr:a0009913003|x:384|y:336';
+parts[0].data = 'nr:a0009913203|x:384|y:336';
+parts[0].data = 'nr:a0009914703|x:384|y:336';
+parts[0].data = 'nr:a0009915203|x:384|y:336';
+parts[0].data = 'nr:a00099180411401|x:384|y:336';
+parts[0].data = 'nr:a0019910270|x:384|y:336';
+parts[0].data = 'nr:a2036370998|x:384|y:336';
